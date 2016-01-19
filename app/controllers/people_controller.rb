f0a41fcb0ebb3f93a11a7a796ace090a5d49af8b
Robert Schäfer <robert.schaefer@student.hpi.de>
2016-01-19 16:53:53 +0000

    fixed filter in controller

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -7,8 +7,8 @@ class PeopleController < ApplicationController
     @people = @people.by_country(params[:country]) if params[:country].present?
     @people = @people.by_city(params[:city]) if params[:city].present?
 
-    @cities = @people.cities
-    @countries = @people.countries
+    @cities = People.cities
+    @countries = People.countries
   end
 
   def show
